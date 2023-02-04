54eeec2bc99e37e1696c7d2608a34a3cc2d93dce
mfux <maxfuchs@protonmail.com>
2023-02-04 16:52:46 +0100

    add project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+"""Package configuration."""
+from setuptools import setup
+
+setup(
+    name="qPusher",
+    version="0.1",
+    packages=["qPusher"],
+    package_dir={"": "src"},
+    install_requires=["pytest==7.1.3", "black==22.10.0"],
+)
---
+++ tests/test_greet.py
@@ -0,0 +1,5 @@
+import qPusher
+
+
+def test_main():
+    assert "hello worl" in qPusher.main()
